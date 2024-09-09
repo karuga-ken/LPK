28c5db130769f35751c2f614398e34a2e4cc3d40
karuga-ken <karugambugua@icloud.com>
2024-09-09 11:49:39 +0300

    history page edit

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,7 +15,7 @@ function About() {
             <p className='text-white text-sm sm:text-base leading-relaxed sm:text-left'>
               Living Positive Kenya (LPK) was founded by Mary “Mum” Wanderi, a Social Worker and advocate, for the past two decades, of children who are neglected, economically disadvantaged, affected by and infected with HIV, and in need of better life opportunities. Through her work, she was able to identify that most of the children that she served had a single mother who was sick and suffering from HIV/AIDS, using drugs, and economically destitute.
 
-              In knowing the importance of improving the lives of these children, Mary started LPK 10 years ago to address the health challenges of these mothers, the neglect of the children, and to improve their lives economically. Mary knows that when an HIV-positive woman is healthy, emotionally stable, and economically empowered, she can take care of her children instead of them being placed in homes.
+              In knowing the importance of improving the lives of these children, Mary started LPK in 2006 to address the health challenges of these mothers, the neglect of the children, and to improve their lives economically. Mary knows that when an HIV-positive woman is healthy, emotionally stable, and economically empowered, she can take care of her children instead of them being placed in homes.
             </p>
           </div>
         </div>
